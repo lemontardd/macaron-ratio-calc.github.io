d592245a366a76049dd6d89b96174951e6b2bf71
lemontardd <85341140+lemontardd@users.noreply.github.com>
2024-05-22 01:04:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function calculateIngredients() {
     let newEggWhites = parseFloat(document.getElementById('newEggWhites').value);
     if (!isNaN(newEggWhites)) {
         let factor = newEggWhites / ratios.eggWhites;
-        document.getElementById('newSugar').textContent = (ratios.sugar * factor).toFixed(2);
-        document.getElementById('newAlmondFlour').textContent = (ratios.almondFlour * factor).toFixed(2);
-        document.getElementById('newPowderedSugar').textContent = (ratios.powderedSugar * factor).toFixed(2);
+        document.getElementById('newSugar').value = (ratios.sugar * factor).toFixed(2);
+        document.getElementById('newAlmondFlour').value = (ratios.almondFlour * factor).toFixed(2);
+        document.getElementById('newPowderedSugar').value = (ratios.powderedSugar * factor).toFixed(2);
     } else {
-        document.getElementById('newSugar').textContent = '';
-        document.getElementById('newAlmondFlour').textContent = '';
-        document.getElementById('newPowderedSugar').textContent = '';
+        document.getElementById('newSugar').value = '';
+        document.getElementById('newAlmondFlour').value = '';
+        document.getElementById('newPowderedSugar').value = '';
     }
 }
 
